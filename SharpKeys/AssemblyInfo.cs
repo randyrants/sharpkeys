c90c52761d92f4ec28a9b099d94431bd2138e17f
Randy <randy@randyrants.com>
2008-12-15 18:19:02 +0000

    Initial check in (v3)

---
+++ SharpKeys/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle("SharpKeys")]
+[assembly: AssemblyDescription("Registry hack for remapping keys")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RandyRants.com")]
+[assembly: AssemblyProduct("SharpKeys")]
+[assembly: AssemblyCopyright("Copyright 2004 - 2009 RandyRants.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("3.0.0.0")]
